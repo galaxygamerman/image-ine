016fa4557b9207eb881c036ef169a83db5c55086
galaxygamerman <mohdimadiqbal@gmail.com>
2025-05-22 22:44:20 +0530

    All graphs are visible together in prog 3

--- 3_StatisticalAnalysis.py
+++ 3_StatisticalAnalysis.py
@@ -8,7 +8,10 @@ images = [
 		("Image 2", cv2.imread('image2.jpg',0))
 ]
 
-for title,image in images:
+figure,axis = plt.subplots(nrows=1,ncols=len(images),figsize=(9,4))
+figure.suptitle('Histogram of Pixel Intensities')
+
+for i,(title,image) in enumerate(images):
 	# Calculate statistics
 	mean = np.mean(image)
 	median = np.median(image)
@@ -24,8 +27,10 @@ for title,image in images:
 
 	# cv2.imshow(title, image)
 	# Plot histogram
-	plt.hist(image.ravel(), bins=256, range=(0, 255), color='gray')
-	plt.title(f'Histogram of Pixel Intensities for {title}')
-	plt.xlabel('Pixel Intensity')
-	plt.ylabel('Frequency')
-	plt.show()
+	subplot = axis[i]
+	subplot.hist(image.ravel(), bins=256, range=(0, 255), color='gray')
+	subplot.set_title(f'Histogram of Pixel Intensities for {title}')
+	subplot.set_xlabel('Pixel Intensity')
+	subplot.set_ylabel('Frequency')
+
+plt.show()
